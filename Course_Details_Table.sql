2a354b193105aaae2b1ce291ee4ec1be5f5794c3
Vincent Vepřek <Kaischatterbox@gmail.com>
2025-04-05 03:33:20 +0100

    Course_Details_Table.sql
    Add new table Course Details where I have inserted fields

---
+++ Course_Details_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Course_Details (
+    Course_ID int,
+    Course_Name varchar(20),
+    Course_Date date,
+    Course_Cost money,
+    Course_Duration date,
+    Manager_Name varchar(20)
+);
