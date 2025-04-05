82d29ee1aa2bfa6effcdbed40fa7d4afe031a8f3
Vincent Vepřek <Kaischatterbox@gmail.com>
2025-04-05 03:40:10 +0100

    Venue_Details_Table.sql
    Added a new table venue details added fields

---
+++ Venue_Details_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Venue_Details_Table (
+    Venue_ID int,
+    Venue_Name varchar(20),
+    Venue_Address varchar(22),
+    Post_Code varchar(10),
+    Venue_Cost money,
+    Venue_Mobile_Number varchar(10),
+    Venue_Email_Address varchar(22)
+);
